ab8220ef31449d23be828de4aca94df1faa8f98b
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-11 19:16:21 +0530

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center bg-green-900 px-16 py-3 text-white sticky top-0'>
+       <strong className='text-xl'>BRIGHTON</strong>
+<div>
+    Home
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
