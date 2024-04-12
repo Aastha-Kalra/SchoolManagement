d1538e72d8749b156579f4e2aa5288040e0d8733
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-12 18:46:22 +0530

    added socials

---
+++ src/components/Social.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='w-full my-10 py-10'>
+<iframe width="100%" height="515" src="https://www.youtube.com/embed/mqE11pGvpmY?si=c_JkvXPZZPNBqQPE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+  )
+}
+
+export default Social
