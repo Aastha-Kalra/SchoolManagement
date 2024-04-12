6ac4bf83c228b33b8a8e2888b9c8d3c794fdeca2
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-12 18:45:58 +0530

    added directorMsg

---
+++ src/components/Director.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Director = () => {
+  return (
+    <div className='flex gap-10 justify-center items-center p-10 my-20'>
+        <div>
+            <img src="https://www.dbgisre.edu.in/wp-content/uploads/2023/07/IMG_0282-1-scaled.webp" alt="" className='rounded-xl p-10' />
+        </div>
+
+        <div>
+       <div>
+       Education should be propagated, absorbed and implemented in such a way that it could be felt physically as well as instinctively. Notable reforms in educational sector have shifted the paradigm from ‘no ledge’ to ‘knowledge’ that certainly is an impinging sign of growth, development, and success of an individual in micro context and nation as a whole in macro conjuncture. It is undoubtedly a meticulous initiative taken by Dev Bhoomi Group of Institutions to make approachable the quality education to every aspirant who wishes to make his/her career a promising and meaningful one.
+       </div>
+       <div className='text-end flex flex-col'>
+       <span className='font-extrabold text-green-500 text-xl'>Mr. Sanjay Bansal</span>
+        <span className='text-red-600 font-bold'>Chairman</span>
+       </div>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Director
