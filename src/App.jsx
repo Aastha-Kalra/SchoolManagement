c56b671ce8fa5c6929d22c3a3dda7c227386ded8
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-19 21:46:14 +0530

    added all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import AboutUS from "./components/AboutUS"
+import ContactUS from "./components/ContactUS"
+import Director from "./components/Director"
+import Facilities from "./components/Facilities"
 import Header from "./components/Header"
+import Highlight from "./components/Highlight"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import Social from "./components/Social"
+import Testimonials from "./components/Testimonials"
 
 function App() {
   return (
     <>
     <Header/>
+    <Navbar/>
+    <Home/>
+    <Facilities/>
+    <AboutUS/>
+    <Director/>
+    <Highlight/>
+    <Testimonials/>
+    <Social/>
+    <ContactUS/>
     </>
   )
 }
