ee05c8772cea00b950bec024ffb18b4d6299aefb
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-19 21:51:47 +0530

    Home file added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 
@@ -17,37 +17,19 @@ const Images = [
 let count = 0;
 const Home = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
-  const slideRef = useRef();
-  const removeAnim = () => {
-    slideRef.current.classList.remove("fade-anim");
-  };
-
-  useEffect(() => {
-    slideRef.current.addEventListener("animationend", removeAnim);
-    startSlider();
-  });
-  const startSlider = () => {
-    setInterval(() => {
-      handleNext();
-      handlePrev();
-    }, 12000);
-  };
   const handlePrev = () => {
     const productsLength = Images.length;
     count = (currentIndex + productsLength - 1) % productsLength;
     setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
   };
   const handleNext = () => {
     count = (count + 1) % Images.length;
     setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
   };
 
   return (
     <div className="w-full pb-4" id="home">
-      <div ref={slideRef} className="w-full relative">
+      <div className="w-full relative">
         <div className="">
           <img
             src={Images[currentIndex].imageUrl}
@@ -59,13 +41,13 @@ const Home = () => {
         <div className="absolute w-full top-80 flex justify-between">
           <button
             onClick={handlePrev}
-            className="text-green-800 font-bolder text-4xl px-4 py-4"
+            className="text-yellow-300 font-bolder text-4xl px-4 py-4"
           >
             <IoIosArrowBack />
           </button>
           <button
             onClick={handleNext}
-            className="text-green-800 font-bolder text-4xl px-4 py-4"
+            className="text-yellow-300 font-bolder text-4xl px-4 py-4"
           >
             <IoIosArrowForward />
           </button>
