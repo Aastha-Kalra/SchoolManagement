cff156cf22ffe190c266eb30397101593cc3ca76
Aastha kalra <aasthakalra2004@gmail.com>
2024-04-19 21:52:07 +0530

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center bg-green-900 px-16 py-3 text-white sticky top-0'>
-       <strong className='text-xl'>BRIGHTON</strong>
-<div>
-    Home
-</div>
-
-
-
+    <div className="flex justify-between items-center bg-green-900 px-16 py-3 text-white sticky top-0 z-50">
+      <strong className="text-xl">BRIGHTON</strong>
+      <div className="flex gap-5">
+        <a href="/">Home</a>
+        <a href="https://aktu.ac.in/syllabus.html" target="blank">
+          Syllabus
+        </a>
+        <a href="https://aktu.ac.in/iccca2020/paper_submission_guidline.html">Guidelines</a>
+        <a href="#contact">Contact Us </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
